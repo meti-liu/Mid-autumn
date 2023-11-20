11e889b400a6529362620383d5730042d245aa9c
Starry <140034460+meti-liu@users.noreply.github.com>
2023-11-20 19:42:24 +0800

    version6
    （final  version）

--- LBR部分代码/首页/script.js
+++ LBR部分代码/首页/script.js
@@ -15,11 +15,19 @@ window.addEventListener('scroll', function ()
   if (value < 1000) 
   { 
     moon.style.marginTop = value * -2+ 'px';
-    left1.style.left = value * -3.5 + 'px';
-    left2.style.left = value * -3.5 + 'px';
-    right1.style.left = value * 3.5 + 'px';
-    right2.style.left = value * 4 + 'px';
+    left1.style.left = value * -1 + 'px';
+    left2.style.left = value * -1 + 'px';
+    right1.style.left = value * 1 + 'px';
+    right2.style.left = value * 1.5 + 'px';
   }
   
 });
+
+let opacity = 1 - value / 1000; // 根据滚动距离计算透明度
+moon.style.opacity = opacity;
+
+// 例如，使用easeInOutQuad缓动函数
+let easedValue = easeInOutQuad(value, 0, 1000, 1000);
+moon.style.marginTop = easedValue * -2 + 'px';
+
 
